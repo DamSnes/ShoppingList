aa978a11ad27103c45d4ab8f65ac7d4daa8d6b60
Alex <517571@mail.ru>
2019-10-28 16:32:19 +0300

    xxxx

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import "./item";
 import { Link } from "react-router-dom";
 
 class App extends React.Component {
- 
+  // написать конструктор, где будет происходить инициализация ЛС
   render() {
-    
-
     return (
       <>
         <div className="header">
-        <div className="name">ShoppingList</div>
-          <Link to={{ pathname: "/shoppingLists" }}className="button1">К списку покупок </Link>
-          
+          <div className="name">ShoppingList</div>
+          <Link to={{ pathname: "/shoppingLists" }} className="button1">
+            К списку покупок{" "}
+          </Link>
         </div>
 
         <div className="list">
--- src/List.js
+++ src/List.js
@@ -2,47 +2,74 @@ import React from "react";
 import "./index.css";
 import { Link } from "react-router-dom";
 
+class Lists extends React.Component {
+  constructor(props) {
+    super(props);
+    // дергаешь локалсторейдж
+    // сделать, чтобы у каждого списка был айдишник, который будешь прокидывать в стейт
+    // в стейте будет поле id
+    // при изменении исходного списка, т.е. items - будем записывать его в локалсторейдж и в стейт
+    // чтобы при последующем открытии этой страницы у нас все данные были в ЛС и, соответственно, на странице
+    this.state = {
+      items: [],
+      text: ""
+    };
+  }
 
-constructor(props) {
-  super(props);
-
-  this.state = {
-    text: "",
-    shoppingList: [],
-    newShoppingList: []
+  onChange = event => {
+    const { value } = event.target;
+    this.setState({ text: value });
   };
-}
 
-const Lists = ({ match, location }) => {
-  const { props } = location;
-  const {
-    params: { id }
-  } = match;
-  
-  
-  
+  handleSubmit = () => {
+    const { text, items } = this.state;
+    if (text) {
+      items.push(text);
+      this.setState({ items, text: "" });
+    }
+  };
 
-  return (
-    <>
-      <div className="header">
-      <div className="name">ShoppingList</div>
-      <Link to={{ pathname: `/ShoppingLists` }} className="button1">Назад</Link>
-      <Link to={{ pathname: `/` }} className="button1">На главную</Link>
-      <div className="window">
-      <p>{`Вы выбрали список ${id}`}</p>
-      <p>{`_Пришли пропсы с customProp = ${
-        props ? props.customProp : "не пришел"
-      }`}</p>
-      </div>
-      </div>
+  handleDelete = index => () => {
+    const { items } = this.state;
+    console.log(index);
+    items.splice(index, 1);
+    console.log(items);
 
-      <div className="content">
+    this.setState({ items });
+  };
+
+  render() {
+    const { match, location } = this.props;
+    const { props } = location;
+    const {
+      params: { id }
+    } = match;
+    const { items, text } = this.state;
+    return (
+      <>
+        <div className="header">
+          <div className="name">ShoppingList</div>
+          <Link to={{ pathname: `/ShoppingLists` }} className="button1">
+            Назад
+          </Link>
+          <Link to={{ pathname: `/` }} className="button1">
+            На главную
+          </Link>
+          <div className="window">
+            <p>{`Вы выбрали список ${id}`}</p>
+            <p>{`_Пришли пропсы с customProp = ${
+              props ? props.customProp : "не пришел"
+            }`}</p>
+          </div>
+        </div>
+
+        <div className="content">
           <input
             className="input"
             placeholder="Добавьте покупку ..."
             value={text}
             onChange={this.onChange}
-          ></input>
+          />
 
           <div className="submitButton" onClick={this.handleSubmit}>
             Добавить
@@ -51,13 +78,13 @@ const Lists = ({ match, location }) => {
 
         <div className="list">
           <ul>
-            {shoppingList.map((item, index) => (
-              <div className="content-list">
-                <div key={index} className="content-item">
+            {items.map((item, index) => (
+              <div className="content-list" key={index}>
+                <div className="content-item">
                   {item}
                   <div
                     className="content-item-delete"
-                    onClick={this.delete(index)}
+                    onClick={this.handleDelete(index)}
                   >
                     X
                   </div>
@@ -66,9 +93,9 @@ const Lists = ({ match, location }) => {
             ))}
           </ul>
         </div>
-      
-    </>
-  );
-};
+      </>
+    );
+  }
+}
 
 export default Lists;
